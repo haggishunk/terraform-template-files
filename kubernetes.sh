8d8e46f2d48835c97fdbf901196fc4ffd7c0ccbd
Travis Mattera <haggishunk@hotmail.com>
2018-02-26 17:10:53 -0800

    Added k8s install

---
+++ kubernetes.sh
@@ -0,0 +1,20 @@
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install docker-ce
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+    apt-key add -
+echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > \
+    /etc/apt/sources.list.d/kubernetes.list
+apt-get -y update
+apt-get -y install \
+    kubeadm=${kube_version}-00 \
+    kubelet=${kube_version}-00 \
+    kubectl=${kube_version}-00
+wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+echo 'source <(kubectl completion bash)' | sudo tee -a .bashrc
