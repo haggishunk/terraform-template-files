cbdc0e39e34ae0b5d624195d3af8b5983a07ae9b
Travis Mattera <haggishunk@hotmail.com>
2018-03-04 12:48:22 -0800

    Using sudo

--- kubernetes.sh
+++ kubernetes.sh
@@ -6,15 +6,15 @@ sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install docker-ce
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install docker-ce
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-    apt-key add -
-echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > \
-    /etc/apt/sources.list.d/kubernetes.list
-apt-get -y update
-apt-get -y install \
+    sudo apt-key add -
+echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | \
+    sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get -y update
+sudo apt-get -y install \
     kubeadm=${kube_version}-00 \
     kubelet=${kube_version}-00 \
     kubectl=${kube_version}-00
