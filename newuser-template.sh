368105a512412fb97e32a0367a24cf7ce8344aaa
Travis Mattera <haggishunk@hotmail.com>
2018-03-04 12:39:39 -0800

    Actually can't use /home/n10 here!

--- newuser-template.sh
+++ newuser-template.sh
@@ -5,11 +5,11 @@ sudo useradd -m ${user}
 sudo chsh -s /bin/bash ${user}
 echo '${user} ALL = (root) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/${user}
 sudo chmod 0440 /etc/sudoers.d/${user}
-sudo mkdir $HOME/.ssh
-sudo cp /root/.ssh/authorized_keys $HOME/.ssh/authorized_keys
-sudo chown -R ${user}:${user} $HOME
-sudo chmod 0700 $HOME/.ssh
-sudo chmod  600 $HOME/.ssh/authorized_keys
+sudo mkdir /home/${user}/.ssh
+sudo cp /root/.ssh/authorized_keys /home/${user}/.ssh/authorized_keys
+sudo chown -R ${user}:${user} /home/${user}
+sudo chmod 0700 /home/${user}/.ssh
+sudo chmod  600 /home/${user}/.ssh/authorized_keys
 
 echo "" | sudo tee -a /etc/ssh/sshd_config
 echo "permitRootLogin no" | sudo tee -a /etc/ssh/sshd_config
