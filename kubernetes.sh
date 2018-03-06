226b75a3cff935b633662e083ef11b5cfb8b6b3d
Travis Mattera <haggishunk@hotmail.com>
2018-03-05 22:03:57 -0800

    Making user part of docker group

--- kubernetes.sh
+++ kubernetes.sh
@@ -6,15 +6,16 @@ sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-sudo apt-get -y -q update
-# sudo apt-get -y upgrade
-sudo apt-get -y -q install docker-ce
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install docker-ce
+sudo usermod -aG docker ${user}
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
     sudo apt-key add -
 echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | \
     sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get -y -q update
-sudo apt-get -y -q install \
+sudo apt-get -y update
+sudo apt-get -y install \
     kubeadm=${kube_version}-00 \
     kubelet=${kube_version}-00 \
     kubectl=${kube_version}-00
