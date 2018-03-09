a1c595c7e3c5c19d9de0a3789dc9e4ebe18680c4
Travis Mattera <haggishunk@hotmail.com>
2018-03-08 20:56:11 -0800

    unquiet for now

--- ceph-install.sh
+++ ceph-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 echo deb https://download.ceph.com/debian/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
-sudo apt-get -qq update
-sudo apt-get -qq -y install ceph-deploy
-sudo apt-get -qq -y install jq
+sudo apt-get -y update
+sudo apt-get -y install ceph-deploy
+sudo apt-get -y install jq
 ssh-keygen -t rsa -b 4096 -f /home/${user}/.ssh/id_rsa -N '' -C '' 
 cat /home/${user}/.ssh/id_rsa.pub | tee -a /home/${user}/.ssh/authorized_keys
 sudo cat /home/${user}/hosts_file | sudo tee -a /etc/hosts
