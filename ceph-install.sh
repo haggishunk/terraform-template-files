e564603f7565469b64e3c885cad38e792cf36e3e
Travis Mattera <haggishunk@hotmail.com>
2018-03-09 10:28:52 -0800

    Moving ssh pri/pub kepair gen to ssh-remote.sh

--- ceph-install.sh
+++ ceph-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install apt-transport-https
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 echo deb https://download.ceph.com/debian/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
 # future improvment to replace above command
@@ -9,8 +10,3 @@ echo deb https://download.ceph.com/debian/ $(lsb_release -sc) main | sudo tee /e
 sudo apt-get -y update
 sudo apt-get -y install ceph-deploy
 sudo apt-get -y install jq
-
-if [[ ! -f /home/${user}/.ssh/id_rsa.pub ]]; then
-    ssh-keygen -t rsa -b 4096 -f /home/${user}/.ssh/id_rsa -N '' -C '' 
-    cat /home/${user}/.ssh/id_rsa.pub | tee -a /home/${user}/.ssh/authorized_keys
-fi
--- ssh-remote.sh
+++ ssh-remote.sh
@@ -5,3 +5,9 @@ cat ~/*.ssh.config | tee ~/.ssh/config
 
 # append hosts to /etc/hosts file
 cat ~/hosts_file | sudo tee -a /etc/hosts
+
+# generate a local public/private ssh keypair if one does not exist
+if [[ ! -f /home/${user}/.ssh/id_rsa.pub ]]; then
+    ssh-keygen -t rsa -b 4096 -f /home/${user}/.ssh/id_rsa -N '' -C '' 
+    cat /home/${user}/.ssh/id_rsa.pub | tee -a /home/${user}/.ssh/authorized_keys
+fi
